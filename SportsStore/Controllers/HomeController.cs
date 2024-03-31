0b4f573923ac6f381359955b3c5ea9b80cf6cdf5
Aleksandr Borovskoy <aleksandr.borovskoy77@gmail.com>
2024-03-31 12:51:48 +0300

    Added Error action method

--- SportsStore/Controllers/HomeController.cs
+++ SportsStore/Controllers/HomeController.cs
@@ -30,5 +30,12 @@ namespace SportsStore.Controllers
                   },
                   CurrentCategory = category,
               });
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View();
+        }
+
     }
 }
